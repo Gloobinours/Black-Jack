c0b747e225e9033404d8cf24812c41f97f915077
Jason H <jasonhailos2150@gmail.com>
2022-06-27 18:44:52 -0400

    main and exec

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,65 +6,21 @@ using namespace std;
 #include "Deck.hpp"
 #include "Player.hpp"
 
-/* 
-    Makes a card given a suit and a value.
-    @param string suit
-    @param int value
-    @return an object Card
-*/
-Card makeCard(string suit, int value)
-{
-    Card card("", suit, value);
-
-    if (value < 11 && value >= 2)
-    {
-        card.setName(to_string(value), suit);
-    }
-    else if (value == 11)
-    {
-        card.setName("Jack", suit);
-    }
-    else if (value == 12)
-    {
-        card.setName("Queen", suit);
-    }
-    else if (value == 13)
-    {
-        card.setName("King", suit);
-    }
-    else if (value == 14)
-    {
-        card.setName("Ace", suit);
-    }
-    else
-    {
-        throw runtime_error("Wrong card value");
-    }
-    
-    return card;
-}
-
-vector<Card> makeDeck()
-{
-    vector<Card> deck;
-
-    for (int i = 2; i <= 14; i++)
-    {
-        deck.push_back(makeCard("Clubs", i));
-        deck.push_back(makeCard("Spades", i));
-        deck.push_back(makeCard("Diamonds", i));
-        deck.push_back(makeCard("Hearts", i));
-    }
-
-    return deck;
-}
-
 int main(int argc, char const *argv[])
 {
-    vector<Card> deck = makeDeck();
-    for (int i = 0; i < deck.size(); i++)
+    //Create player1
+    string name;
+    cout << "What is your name?" << endl;
+    cin >> name;
+    Player player1(name, 500);
+
+    string prompt;
+    cin >> prompt;
+    while (prompt.toupper() != "STOP")
     {
-        cout << deck[i] << endl;
+        /* code */
     }
+    
+
     return 0;
 }
