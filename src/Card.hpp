6859f145c6716035b0c5cab520ab676cc76aa8e8
Louan Flammanc <kusami.louan@gmail.com>
2022-06-28 15:01:39 -0400

    add comments to Card.hpp

--- src/Card.hpp
+++ src/Card.hpp
@@ -10,13 +10,42 @@ class Card
     int value;
 
 public:
+    /**
+     * Default constructor
+     * Sets the name to "placeholder, placeholder" and value to 0
+     * @see value
+     * @see name
+     */
     Card();
+
+    /**
+     * Second construcor
+     * @param string s, name of the suit
+     * @param int v, value of the card
+     */
     Card(string s, int v);
+
+    /**
+     * @return name of the card
+     */
     string getName();
+
+    /**
+     * @return suit of the card
+     */
     string getSuit();
+
+    /**
+     * @return value of the card
+     */
     int getValue();
+
+    /**
+     * Sets the name of the card
+     * @param string n, value for the card
+     * @param string s, suit of the card
+     */
     void setName(string n, string s);
-    Card makeCard(string suit, int value);
 };
 
 ostream& operator<<(ostream& os, Card& c);
